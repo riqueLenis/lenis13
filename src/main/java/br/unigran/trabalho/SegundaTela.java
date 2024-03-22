372e054b33f353969ae0b42adfb6bc813fc00bd4
Laboratorio <laboratorio@lab.unigran>
2024-03-21 20:26:41 -0400

    first

---
+++ src/main/java/br/unigran/trabalho/SegundaTela.java
@@ -0,0 +1,16 @@
+
+package br.unigran.trabalho;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JDialog;
+
+
+public class SegundaTela extends JDialog implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+       
+    }
+    
+}
---
+++ src/main/java/br/unigran/trabalho/Trabalho.java
@@ -0,0 +1,16 @@
+package br.unigran.trabalho;
+
+import java.awt.HeadlessException;
+import javax.swing.JFrame;
+
+
+public class Trabalho extends JFrame {
+    private int tam;
+    public Trabalho() throws HeadlessException{
+    
+    }
+    public static void main(String[] args) {
+        
+    }
+
+}
